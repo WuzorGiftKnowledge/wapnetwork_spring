9d46abc9b07961b0cccb01b31b26757361233988
unknown <WuzorGiftKnowledge>
2021-08-20 15:48:01 +0100

    heroku

--- src/main/java/com/example/demo/utils/PasswordEncode.java
+++ src/main/java/com/example/demo/utils/PasswordEncode.java
@@ -1,9 +1,9 @@
-//package com.example.demo.utils;
+package com.example.demo.utils;
 //
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //
-//public class PasswordEncode {
-//
+public class PasswordEncode {
+
 //	public static BCryptPasswordEncoder encode(){
 //		return new BCryptPasswordEncoder();
 //	}
@@ -11,5 +11,5 @@
 //	public static boolean checkPassword(String oldPassword, String encryptedPassword) {
 //		BCryptPasswordEncoder encoder = PasswordEncode.encode();
 //		return encoder.matches(oldPassword, encryptedPassword);
-//	}
-//}
+	
+}
