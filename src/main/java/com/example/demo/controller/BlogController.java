965ee6391ca4ba9c8e7d6ac2f4210a06abc28177
unknown <WuzorGiftKnowledge>
2021-08-24 15:12:44 +0100

    swagger

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -21,6 +21,7 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,12 +40,13 @@ public class BlogController {
      private BlogService blogService;
      
       @GetMapping("/blogview")
+     
     public String blogview() {
         return "blogview";
 }
-    
+         @Transactional
       @GetMapping("/blog")
-    public String blog(Model model) {
+       public String blog(Model model) {
         model.addAttribute("blogs", blogService.getAllAprovedBlog());
         return "blogs";
 }
